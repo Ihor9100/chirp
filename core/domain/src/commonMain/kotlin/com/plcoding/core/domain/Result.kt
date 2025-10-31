9f5599a5353a4b5f6dd993e043df01203219b441
IhorBohdanovskyi <ihor.bohdanovskyi@alty.co>
2025-10-31 11:46:03 +0200

    added Result utils classes (part2)

--- core/domain/src/commonMain/kotlin/com/plcoding/core/domain/Result.kt
+++ core/domain/src/commonMain/kotlin/com/plcoding/core/domain/Result.kt
@@ -2,9 +2,11 @@ package com.plcoding.core.domain
 
 import com.plcoding.core.domain.utils.Error
 
+typealias Empty<E> = Result<Unit, E>
+
 sealed interface Result<out D, out E : Error> {
   data class Success<out D>(val data: D) : Result<D, Nothing>
-  data class Failure<out E: Error>(val error: E) : Result<Nothing, E>
+  data class Failure<out E : Error>(val error: E) : Result<Nothing, E>
 }
 
 fun <D, E : Error, R> Result<D, E>.map(transform: (D) -> R): Result<R, E> {
@@ -12,6 +14,10 @@ fun <D, E : Error, R> Result<D, E>.map(transform: (D) -> R): Result<R, E> {
     is Result.Failure -> this
     is Result.Success -> Result.Success(transform(data))
   }
+}
+
+fun <D, E : Error> Result<D, E>.asEmpty(): Empty<E> {
+ return map {  }
 }
 
 fun <D, E : Error> Result<D, E>.onSuccess(action: (D) -> Unit): Result<D, E> {
