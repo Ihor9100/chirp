beeadeb609530122c3c58f194928d556336eb482
IhorBohdanovskyi <ihor.bohdanovskyi@alty.co>
2025-11-08 17:17:44 +0200

    networking utils part2

--- core/data/src/commonMain/kotlin/com/plcoding/core/data/networking/HttpClientExt.kt
+++ core/data/src/commonMain/kotlin/com/plcoding/core/data/networking/HttpClientExt.kt
@@ -1,5 +1,27 @@
 package com.plcoding.core.data.networking
 
+import com.plcoding.core.domain.utils.DataError
+import com.plcoding.core.domain.Result
+import io.ktor.client.call.NoTransformationFoundException
+import io.ktor.client.call.body
+import io.ktor.client.statement.HttpResponse
+
+suspend inline fun <reified T> responseToResult(
+  httpResponse: HttpResponse,
+): Result<T, DataError.Remote> {
+  return when (httpResponse.status.value) {
+    in 200..299 -> {
+      try {
+        Result.Success(httpResponse.body<T>())
+      } catch (e: NoTransformationFoundException) {
+        Result.Failure(e)
+      }
+    }
+    // TODO:
+    else -> throw IllegalStateException()
+  }
+}
+
 fun constructRoute(route: String): String {
   return when {
     route.contains(BASE_URL) -> route
