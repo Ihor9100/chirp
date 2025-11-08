e6f16bc075961eb6bff50c59d8479ff0d1aa1b5b
IhorBohdanovskyi <ihor.bohdanovskyi@alty.co>
2025-11-08 22:47:28 +0200

    networking utils part3

--- core/data/src/commonMain/kotlin/com/plcoding/core/data/networking/HttpClientExt.kt
+++ core/data/src/commonMain/kotlin/com/plcoding/core/data/networking/HttpClientExt.kt
@@ -1,10 +1,21 @@
 package com.plcoding.core.data.networking
 
-import com.plcoding.core.domain.utils.DataError
 import com.plcoding.core.domain.Result
+import com.plcoding.core.domain.utils.DataError
 import io.ktor.client.call.NoTransformationFoundException
 import io.ktor.client.call.body
 import io.ktor.client.statement.HttpResponse
+
+expect suspend fun <T> platformSafeCall(
+  execute: suspend () -> HttpResponse,
+  handleResponse: suspend (HttpResponse) -> Result<T, DataError.Remote>
+): Result<T, DataError.Remote>
+
+suspend inline fun <reified T> safeCall(
+  noinline execute: suspend () -> HttpResponse,
+): Result<T, DataError.Remote> {
+  return platformSafeCall(execute, ::responseToResult)
+}
 
 suspend inline fun <reified T> responseToResult(
   httpResponse: HttpResponse,
@@ -13,12 +24,21 @@ suspend inline fun <reified T> responseToResult(
     in 200..299 -> {
       try {
         Result.Success(httpResponse.body<T>())
-      } catch (e: NoTransformationFoundException) {
-        Result.Failure(e)
+      } catch (_: NoTransformationFoundException) {
+        Result.Failure(DataError.Remote.SERIALIZATION)
       }
     }
-    // TODO:
-    else -> throw IllegalStateException()
+    400 -> Result.Failure(DataError.Remote.BAD_REQUEST)
+    401 -> Result.Failure(DataError.Remote.UNAUTHORIZED)
+    403 -> Result.Failure(DataError.Remote.FORBIDDEN)
+    404 -> Result.Failure(DataError.Remote.NOT_FOUND)
+    408 -> Result.Failure(DataError.Remote.REQUEST_TIMEOUT)
+    409 -> Result.Failure(DataError.Remote.CONFLICT)
+    413 -> Result.Failure(DataError.Remote.PAYLOAD_TOO_LARGE)
+    429 -> Result.Failure(DataError.Remote.TOO_MANY_REQUESTS)
+    500 -> Result.Failure(DataError.Remote.SERVER_ERROR)
+    503 -> Result.Failure(DataError.Remote.SERVER_UNAVAILABLE)
+    else -> Result.Failure(DataError.Remote.UNKNOWN)
   }
 }
 
--- core/domain/src/commonMain/kotlin/com/plcoding/core/domain/utils/DataError.kt
+++ core/domain/src/commonMain/kotlin/com/plcoding/core/domain/utils/DataError.kt
@@ -2,7 +2,18 @@ package com.plcoding.core.domain.utils
 
 sealed interface DataError : Error {
   enum class Remote : Error {
-
+    SERIALIZATION,
+    BAD_REQUEST,
+    UNAUTHORIZED,
+    FORBIDDEN,
+    NOT_FOUND,
+    REQUEST_TIMEOUT,
+    CONFLICT,
+    PAYLOAD_TOO_LARGE,
+    TOO_MANY_REQUESTS,
+    SERVER_ERROR,
+    SERVER_UNAVAILABLE,
+    UNKNOWN,
   }
 
   enum class Local : Error {
