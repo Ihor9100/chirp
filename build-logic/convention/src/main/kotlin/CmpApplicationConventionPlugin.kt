5362b017d8ca59079dea489e79ac6a02d6b00f7a
IhorBohdanovskyi <ihor.bohdanovskyi@alty.co>
2025-11-13 08:59:29 +0200

    trying to fix @Preview annotations for JetBrains compose part1

--- build-logic/convention/src/main/kotlin/CmpApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/CmpApplicationConventionPlugin.kt
@@ -1,18 +1,22 @@
+import com.android.build.api.dsl.ApplicationExtension
 import com.plcoding.buildlogic.convention.configureAndroidTarget
+import com.plcoding.buildlogic.convention.configureCompose
 import com.plcoding.buildlogic.convention.configureIosTarget
 import com.plcoding.buildlogic.convention.libs
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
 
-class CmpApplicationConventionPlugin : AndroidApplicationComposeConventionPlugin() {
+class CmpApplicationConventionPlugin : AndroidApplicationConventionPlugin() {
 
   override fun apply(target: Project) {
     super.apply(target)
 
     with(target) {
+      pluginManager.apply("org.jetbrains.kotlin.plugin.compose")
       pluginManager.apply("org.jetbrains.kotlin.multiplatform")
       pluginManager.apply("org.jetbrains.compose")
 
+      configureCompose(extensions.getByType(ApplicationExtension::class.java))
       configureAndroidTarget()
       configureIosTarget(baseName = "ComposeApp", isStatic = true)
     }
