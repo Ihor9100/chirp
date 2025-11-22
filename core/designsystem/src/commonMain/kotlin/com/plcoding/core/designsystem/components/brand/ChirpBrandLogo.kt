127c7faf111b1a5ea78d7e520e806bb493d132ec
IhorBohdanovskyi <ihor.bohdanovskyi@alty.co>
2025-11-22 16:26:31 +0200

    networking utils part3

---
+++ core/designsystem/src/commonMain/kotlin/com/plcoding/core/designsystem/components/brand/ChirpBrandLogo.kt
@@ -0,0 +1,21 @@
+package com.plcoding.core.designsystem.components.brand
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import chirp.core.designsystem.generated.resources.Res
+import chirp.core.designsystem.generated.resources.ic_logo_chirp
+import org.jetbrains.compose.resources.vectorResource
+
+@Composable
+fun ChirBrandLogo(
+  modifier: Modifier = Modifier,
+) {
+  Icon(
+    imageVector = vectorResource(Res.drawable.ic_logo_chirp),
+    contentDescription = null,
+    modifier = modifier,
+    tint = MaterialTheme.colorScheme.primary
+  )
+}
--- core/designsystem/src/commonMain/kotlin/com/plcoding/core/designsystem/components/layout/ChirAdaptiveFormLayout.kt
+++ core/designsystem/src/commonMain/kotlin/com/plcoding/core/designsystem/components/layout/ChirAdaptiveFormLayout.kt
@@ -1,18 +1,30 @@
 package com.plcoding.core.designsystem.components.layout
 
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.plcoding.core.designsystem.components.brand.ChirBrandLogo
+import com.plcoding.core.designsystem.components.brand.ChirBrandTitle
+import com.plcoding.core.designsystem.components.surface.ChirpSurface
+import com.plcoding.core.designsystem.style.ChirTheme
 import com.plcoding.core.designsystem.style.extended
 import com.plcoding.core.presentation.utils.DeviceConfiguration
 import com.plcoding.core.presentation.utils.getDeviceConfiguration
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import kotlin.math.log
 
 @Composable
 fun ChirAdaptiveFormLayout(
+  modifier: Modifier = Modifier,
   deviceConfiguration: DeviceConfiguration = getDeviceConfiguration(),
   logo: @Composable () -> Unit,
   title: String,
-  error: String,
+  error: String?,
   form: @Composable ColumnScope.() -> Unit,
 ) {
   val titleColor = if (deviceConfiguration == DeviceConfiguration.MOBILE_LANDSCAPE) {
@@ -23,7 +35,18 @@ fun ChirAdaptiveFormLayout(
 
   when (deviceConfiguration) {
     DeviceConfiguration.MOBILE_PORTRAIT -> {
-      TODO()
+      ChirpSurface(
+        modifier = modifier,
+        logo = logo,
+      ) {
+        ChirBrandTitle(
+          modifier = Modifier,
+          title = title,
+          titleColor = titleColor,
+          error = error,
+        )
+        form()
+      }
     }
     DeviceConfiguration.MOBILE_LANDSCAPE -> {
       TODO()
@@ -33,5 +56,50 @@ fun ChirAdaptiveFormLayout(
     DeviceConfiguration.DESKTOP -> {
       TODO()
     }
+  }
+}
+
+
+@Composable
+@Preview
+fun ChirpAdaptiveFormPortraitLightPreview() {
+  ChirpAdaptiveFormPreview(
+    isDarkTheme = false,
+    deviceConfiguration = DeviceConfiguration.MOBILE_PORTRAIT,
+  )
+}
+
+@Composable
+@Preview
+fun ChirpAdaptiveFormPortraitDarkPreview() {
+  ChirpAdaptiveFormPreview(
+    isDarkTheme = true,
+    deviceConfiguration = DeviceConfiguration.MOBILE_PORTRAIT,
+  )
+}
+
+@Composable
+fun ChirpAdaptiveFormPreview(
+  isDarkTheme: Boolean,
+  deviceConfiguration: DeviceConfiguration,
+) {
+  ChirTheme(isDarkTheme) {
+    ChirAdaptiveFormLayout(
+      modifier = Modifier
+        .fillMaxSize(),
+      deviceConfiguration = deviceConfiguration,
+      logo = {
+        ChirBrandLogo()
+      },
+      title = "Hello World",
+      error = null,
+      form = {
+        Text(
+          text = "Test",
+          color = MaterialTheme.colorScheme.extended.textPrimary,
+          style = MaterialTheme.typography.bodySmall,
+        )
+      }
+    )
   }
 }
