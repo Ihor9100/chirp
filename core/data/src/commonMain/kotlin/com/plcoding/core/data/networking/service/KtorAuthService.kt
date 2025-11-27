26fbe28f6a45906b97698b878ecdda06fe13c6d5
IhorBohdanovskyi <ihor.bohdanovskyi@alty.co>
2025-11-27 11:13:10 +0200

    AuthService part1

---
+++ core/data/src/commonMain/kotlin/com/plcoding/core/data/networking/service/KtorAuthService.kt
@@ -0,0 +1,24 @@
+package com.plcoding.core.data.networking.service
+
+import com.plcoding.core.data.networking.model.RegisterRequest
+import com.plcoding.core.data.networking.post
+import com.plcoding.core.domain.Result
+import com.plcoding.core.domain.networking.service.AuthService
+import com.plcoding.core.domain.utils.DataError
+import io.ktor.client.HttpClient
+
+class KtorAuthService(
+  private val httpClient: HttpClient,
+) : AuthService {
+
+  override suspend fun register(
+    username: String,
+    email: String,
+    password: String
+  ): Result<Unit, DataError.Remote> {
+    return httpClient.post<RegisterRequest, Unit>(
+      route = "/auth/register",
+      request = RegisterRequest(username, email, password),
+    )
+  }
+}
---
+++ core/domain/src/commonMain/kotlin/com/plcoding/core/domain/networking/service/AuthService.kt
@@ -0,0 +1,13 @@
+package com.plcoding.core.domain.networking.service
+
+import com.plcoding.core.domain.Result
+import com.plcoding.core.domain.utils.DataError
+
+interface AuthService {
+
+  suspend fun register(
+    username: String,
+    email: String,
+    password: String,
+  ): Result<Unit, DataError.Remote>
+}
