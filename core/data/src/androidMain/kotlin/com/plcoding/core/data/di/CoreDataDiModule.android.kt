16d87a8418815de95796ac65d5d10bbbc6720e25
IhorBohdanovskyi <ihor.bohdanovskyi@alty.co>
2025-12-07 21:02:50 +0200

    CryptographyRepository for Android side

--- core/data/src/androidMain/kotlin/com/plcoding/core/data/di/CoreDataDiModule.android.kt
+++ core/data/src/androidMain/kotlin/com/plcoding/core/data/di/CoreDataDiModule.android.kt
@@ -2,13 +2,18 @@ package com.plcoding.core.data.di
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import com.plcoding.core.data.repository.CryptographyDataRepository
 import com.plcoding.core.data.tools.createDataStore
+import com.plcoding.core.domain.repository.CryptographyRepository
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttp
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 actual val platformCoreDataDiModule = module {
   single<HttpClientEngine> { OkHttp.create() }
   single<DataStore<Preferences>> { createDataStore(androidContext()) }
+  singleOf(::CryptographyDataRepository) bind CryptographyRepository::class
 }
---
+++ core/data/src/iosMain/kotlin/com/plcoding/core/data/repository/CryptographyDataRepository.ios.kt
@@ -0,0 +1,14 @@
+package com.plcoding.core.data.repository
+
+import com.plcoding.core.domain.repository.CryptographyRepository
+
+class CryptographyDataRepository : CryptographyRepository {
+
+  override fun encrypt(string: String): String {
+    TODO("Not yet implemented")
+  }
+
+  override fun decrypt(string: String): String {
+    TODO("Not yet implemented")
+  }
+}
