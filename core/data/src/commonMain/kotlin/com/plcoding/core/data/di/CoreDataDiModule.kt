3aa6a56e8f57f28f65626bff2a7c787dc3c0cf86
IhorBohdanovskyi <ihor.bohdanovskyi@alty.co>
2025-12-09 17:53:03 +0200

    dependency fix

--- core/data/src/commonMain/kotlin/com/plcoding/core/data/di/CoreDataDiModule.kt
+++ core/data/src/commonMain/kotlin/com/plcoding/core/data/di/CoreDataDiModule.kt
@@ -1,14 +1,14 @@
 package com.plcoding.core.data.di
 
 import com.plcoding.core.data.logger.KermitLogger
-import com.plcoding.core.data.tools.HttpClientFactory
 import com.plcoding.core.data.mapper.AuthInfoMapper
 import com.plcoding.core.data.mapper.UserMapper
-import com.plcoding.core.data.repository.remote.AuthRemoteDataRepository
 import com.plcoding.core.data.repository.local.PreferencesLocalDataRepository
+import com.plcoding.core.data.repository.remote.AuthRemoteDataRepository
+import com.plcoding.core.data.tools.HttpClientFactory
 import com.plcoding.core.domain.logger.ChirpLogger
-import com.plcoding.core.domain.repository.remote.AuthRemoteRepository
 import com.plcoding.core.domain.repository.local.PreferencesLocalRepository
+import com.plcoding.core.domain.repository.remote.AuthRemoteRepository
 import io.ktor.client.HttpClient
 import kotlinx.serialization.json.Json
 import org.koin.core.module.Module
@@ -24,7 +24,7 @@ val coreDataDiModule = module {
 
   single<Json> { Json { ignoreUnknownKeys = true } }
   single<ChirpLogger> { KermitLogger }
-  single<HttpClient> { HttpClientFactory(get(), get()).create(get()) }
+  single<HttpClient> { HttpClientFactory(get(), get(), get()).create(get()) }
 
   singleOf(::AuthRemoteDataRepository) bind AuthRemoteRepository::class
   singleOf(::PreferencesLocalDataRepository) bind PreferencesLocalRepository::class
