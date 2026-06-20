6ec1704f271b6495e21cad8ff267079c5fbaa219
IhorBohdanovskyi <ihor.bohdanovskyi@alty.co>
2026-06-20 07:06:13 +0300

    chat details part4

--- feature/chat/data/src/commonMain/kotlin/com/plcoding/feature/chat/data/repository/ChatDataRepository.kt
+++ feature/chat/data/src/commonMain/kotlin/com/plcoding/feature/chat/data/repository/ChatDataRepository.kt
@@ -3,26 +3,13 @@ package com.plcoding.feature.chat.data.repository
 import com.plcoding.core.domain.result.DataError
 import com.plcoding.core.domain.result.Empty
 import com.plcoding.core.domain.result.Result
-import com.plcoding.core.domain.result.asEmpty
 import com.plcoding.core.domain.result.flatMap
-import com.plcoding.core.domain.result.map
-import com.plcoding.core.domain.result.onSuccess
 import com.plcoding.feature.chat.data.datasource.local.ChatsLocalDataSource
 import com.plcoding.feature.chat.data.datasource.remote.ChatsRemoteDataSource
-import com.plcoding.feature.chat.data.mapper.ChatAndMembersRelationMapper
-import com.plcoding.feature.chat.data.mapper.ChatEntityMapper
-import com.plcoding.feature.chat.data.mapper.ChatMemberEntityMapper
-import com.plcoding.feature.chat.data.mapper.ChatMessageEntityMapper
-import com.plcoding.feature.chat.data.mapper.ChatsAndMembersEntityMapper
-import com.plcoding.feature.chat.database.dao.ChatAndMemberDao
-import com.plcoding.feature.chat.database.dao.ChatMembersDao
-import com.plcoding.feature.chat.database.dao.ChatMessagesDao
-import com.plcoding.feature.chat.database.dao.ChatsDao
 import com.plcoding.feature.chat.domain.model.Chat
 import com.plcoding.feature.chat.domain.model.ChatMember
 import com.plcoding.feature.chat.domain.repository.ChatRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
 class ChatDataRepository(
   private val localDataSource: ChatsLocalDataSource,
