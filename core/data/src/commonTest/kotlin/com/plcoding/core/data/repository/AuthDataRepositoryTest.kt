ddac9d1f25fbeb8672f137ac30e667065dc8a5b7
IhorBohdanovskyi <ihor.bohdanovskyi@alty.co>
2026-09-11 18:01:27 +0300

    tests part4

--- core/data/src/commonTest/kotlin/com/plcoding/core/data/repository/AuthDataRepositoryTest.kt
+++ core/data/src/commonTest/kotlin/com/plcoding/core/data/repository/AuthDataRepositoryTest.kt
@@ -4,21 +4,16 @@ import com.plcoding.core.data.model.RegisterRequestDto
 import com.plcoding.core.domain.result.Result
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.mock.MockEngine
+import io.ktor.client.engine.mock.respond
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.HttpRequestData
-import io.ktor.client.request.HttpResponseData
 import io.ktor.client.request.header
 import io.ktor.content.TextContent
 import io.ktor.http.ContentType
-import io.ktor.http.Headers
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
-import io.ktor.http.HttpProtocolVersion
-import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
-import io.ktor.util.date.GMTDate
-import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.test.runTest
 import kotlinx.serialization.json.Json
 import kotlin.test.Test
@@ -39,16 +34,7 @@ class AuthDataRepositoryTest {
     )
     val mockEngine = MockEngine {
       httpRequestData = it
-      HttpResponseData(
-        statusCode = HttpStatusCode.Created,
-        requestTime = GMTDate(requestTime.toEpochMilliseconds()),
-        headers = Headers.build {
-          set(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-        },
-        version = HttpProtocolVersion.HTTP_2_0,
-        body = Unit,
-        callContext = currentCoroutineContext(),
-      )
+      respond(content = "")
     }
     val httpClient = HttpClient(mockEngine) {
       install(ContentNegotiation) {
@@ -68,7 +54,6 @@ class AuthDataRepositoryTest {
 
     assertEquals("/api/auth/register", httpRequestData?.url?.encodedPath)
     assertEquals(HttpMethod.Post, httpRequestData?.method)
-    assertEquals(ContentType.Application.Json.toString(), httpRequestData?.headers?.get(HttpHeaders.ContentType))
     val body = httpRequestData?.body as? TextContent
     val bodyText = body?.text
     assertEquals(registerRequestDto, bodyText?.let { Json.decodeFromString<RegisterRequestDto>(it) })
