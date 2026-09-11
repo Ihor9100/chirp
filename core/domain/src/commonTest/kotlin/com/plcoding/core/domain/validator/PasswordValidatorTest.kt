4a526c9c1eea9183882622bfe02a457ff4f99b9b
IhorBohdanovskyi <ihor.bohdanovskyi@alty.co>
2026-09-11 11:15:24 +0300

    tests part2

--- core/domain/src/commonTest/kotlin/com/plcoding/core/domain/validator/PasswordValidatorTest.kt
+++ core/domain/src/commonTest/kotlin/com/plcoding/core/domain/validator/PasswordValidatorTest.kt
@@ -1,5 +1,38 @@
 package com.plcoding.core.domain.validator
 
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
 class PasswordValidatorTest {
 
+  @Test
+  fun `valid password returns true`() {
+    assertTrue(PasswordValidator.validate("Naruto!19890702"))
+  }
+
+  @Test
+  fun `no digit returns false`() {
+    assertFalse(PasswordValidator.validate("Narutoooo"))
+  }
+
+  @Test
+  fun `no uppercase returns false`() {
+    assertFalse(PasswordValidator.validate("naruto19890702"))
+  }
+
+  @Test
+  fun `short password returns false`() {
+    assertFalse(PasswordValidator.validate("Naruto1"))
+  }
+
+  @Test
+  fun `9 chars returns true`() {
+    assertTrue(PasswordValidator.validate("Naruto111"))
+  }
+
+  @Test
+  fun `8 chars returns false`() {
+    assertFalse(PasswordValidator.validate("Naruto11"))
+  }
 }
